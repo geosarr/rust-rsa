43096a4a102d029c73891e3fea9ccc5f530d7da0
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-10-04 22:01:59 +0200

    added factor_two unittest and Miller-Rabin test

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,26 @@
 // use num_bigint::BigUint;
 // use num_bigint::{ToBigInt, RandBigInt, BigUint};
 use num_bigint::{RandBigInt, BigUint};
-use num_traits::One;
+use num_traits::{Zero, One};
 
 fn main(){
+    let bit_size: u64 = 500; 
+    let k_mil_rab: u32 = 3;
+    gen_prime(bit_size, k_mil_rab);
+}
 
-    while true {
-        let bit_size: u64 = 256; 
+fn gen_prime(bit_size: u64, k: u32) -> BigUint{
+
+    loop {
+        
         let num: BigUint = gen_random_odd_biguint(bit_size);
         // cloning to prevent errors due to data move and borrowing
-        if !is_prime(num.clone()){
-            println!("not prime");
-            let num = gen_random_odd_biguint(bit_size);
-            println!("{}", num);
-        } else{
+        if is_prime(num.clone(), k){
             println!("YUPI-HAPPY {} is probably a prime number", num);
-            break;
-        }
+            return num
+        } 
     }
- 
+    
 }
 
 fn gen_random_odd_biguint(bit_size: u64) -> BigUint{
@@ -33,8 +35,12 @@ fn gen_random_odd_biguint(bit_size: u64) -> BigUint{
     num
 }
 
-fn is_prime(b_num: BigUint) -> bool{
-    if !little_fermat(b_num){
+fn is_prime(b_num: BigUint, k: u32) -> bool{
+    if !little_fermat(b_num.clone()){
+        return false
+    }
+
+    if !miller_rabin(b_num.clone(), k){
         return false
     }
     true
@@ -46,4 +52,70 @@ fn little_fermat(bnum: BigUint) -> bool{
     let one: BigUint = One::one();
     let remainder =  BigUint::modpow(&lower_num, &(&bnum - &one), &bnum);
     remainder == one
+}
+
+fn miller_rabin(num: BigUint, k: u32) -> bool{
+    let one: BigUint = One::one();
+    let two = &one + &one;
+    for _ in 1..k{
+        let mut rng = rand::thread_rng();
+        let rand_num = rng.gen_biguint_range(&two, &(&num - &two)); 
+        if is_miller_witness(num.clone(), rand_num){
+            return false
+        }
+    }
+    true
+}
+
+fn is_miller_witness(num: BigUint, rand_num: BigUint) -> bool{
+    let one: BigUint = One::one();
+    // let two: u32 = 2;
+    let two = &one + &one;
+    let (s,d) = factor_two(num.clone() - one.clone());   
+    let mut x = BigUint::modpow(&rand_num, &d ,&num);
+    if x == one || x == num.clone() - one.clone(){
+        return false
+    } else {
+        for _ in 1..s - 1{
+            x = BigUint::modpow(&x, &two , &num);
+            if x == num.clone() - one.clone(){
+                return false
+            }
+        }
+        true
+    }
+}
+
+fn factor_two(num: BigUint) -> (u32, BigUint){
+    let mut s: u32 = 1;
+    let zero : BigUint = Zero::zero();
+    let one: BigUint = One::one();
+    let two = &one + &one;
+    let mut remainder = BigUint::modpow(&num, &one, &BigUint::pow(&two, s));
+    while remainder == zero {
+        s = s + 1;
+        remainder = BigUint::modpow(&num, &one, &BigUint::pow(&two, s));
+    }
+    (s-1, num.clone()/BigUint::pow(&two, s-1))
+}
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_factor_two(){
+        for _ in 1..100000{
+            let mut rng = rand::thread_rng();
+            let num = rng.gen_biguint(256);
+            let one: BigUint = One::one();
+            let zero : BigUint = Zero::zero();
+            let two = &one + &one;
+            let (s,d) = factor_two(num.clone());
+            assert_eq!(zero, BigUint::modpow(&num, &one, &BigUint::pow(&two, s)));
+            assert_ne!(zero, BigUint::modpow(&num, &one, &BigUint::pow(&two, s+1)));
+            assert_eq!(num.clone(), BigUint::pow(&two, s) * d);
+        }
+    }
 }

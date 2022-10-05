4c1bee37ad72cd021e4daf5e41ee069caba1968c
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-10-05 21:06:41 +0200

    added clap argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 mod utils;
 use utils::{gen_prime};
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Number of bits of the numbers to generate
+    #[arg(short, long)]
+    bit_size: u64,
+
+    /// Number of times to run Miller-Rabin test
+    #[arg(short, long, default_value_t = 1)]
+    k_mil_rab: u32,
+}
 
 fn main(){
-    let bit_size: u64 = 500; 
-    let k_mil_rab: u32 = 3;
-    gen_prime(bit_size, k_mil_rab);
+    let args = Args::parse();
+    println!("");
+    println!("Code launched with the following arguments {:#?}", args);
+
+    let bit_size: u64= args.bit_size;
+    let k_mil_rab: u32 = args.k_mil_rab;
+    gen_prime(bit_size, k_mil_rab);    
 }

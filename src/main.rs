b73295ce83e040fb76bf20010f4e2f3ba6077afc
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-10-06 20:53:52 +0200

    puclic key generation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod utils;
-use utils::{gen_prime};
+use utils::{gen_prime, gen_rand_inverses_below};
+use num_bigint::BigUint;
+use num_traits::One;
 use clap::Parser;
 use std::thread;
 
@@ -14,6 +16,7 @@ struct Args {
     k_mil_rab: u32,
 }
 
+
 fn main(){
     let args = Args::parse();
     println!("");
@@ -22,11 +25,16 @@ fn main(){
     let bit_size: u64= args.bit_size;
     let k_mil_rab: u32 = args.k_mil_rab;
 
-    println!("Generating the prime numbers ...");
+    println!("\nGenerating the prime numbers ...");
     let handle = thread::spawn(move || {
         (gen_prime(bit_size, k_mil_rab), gen_prime(bit_size, k_mil_rab))
         }
     );
     let (p,q) = handle.join().unwrap();    
-    println!("p = {} \nq = {} \nn = {}", p.clone(), q.clone(), p.clone() * q.clone());
+
+    let one : BigUint = One::one();
+    let euler_ind = (p.clone()-one.clone()) * (q.clone()-one.clone());
+    let (e, _) = gen_rand_inverses_below(euler_ind);
+
+    println!("\nYour public key is the pair [ \nn = {}  \n\ne = {} \n]", p*q, e);
 }

68483e44515cf235c8c936db5dd8750efb0dc955
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-10-07 17:19:50 +0200

    hexadecimal conversion

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,72 @@
 #[cfg(test)]
 mod unit_tests;
-use num_bigint::{RandBigInt, BigInt, BigUint, Sign::Plus, Sign::Minus};
+use num_bigint::{RandBigInt, BigInt, BigUint, ToBigUint, Sign::Plus, Sign::Minus};
 use num_traits::{Zero, One};
 use std::collections::HashMap;
 use std::cmp::{min, max};
 use std::mem::replace;
+use std::string::String;
+use std::vec::Vec;
+use std::char;
+
+// SOME CONSTANTS
+pub fn hex_from_uint() -> HashMap<BigUint, char> { 
+    HashMap::from([
+        (0.to_biguint().unwrap(), '0'), 
+        (1.to_biguint().unwrap(), '1'), 
+        (2.to_biguint().unwrap(), '2'), 
+        (3.to_biguint().unwrap(), '3'),  
+        (4.to_biguint().unwrap(), '4'), 
+        (5.to_biguint().unwrap(), '5'), 
+        (6.to_biguint().unwrap(), '6'), 
+        (7.to_biguint().unwrap(), '7'), 
+        (8.to_biguint().unwrap(), '8'), 
+        (9.to_biguint().unwrap(), '9'), 
+        (10.to_biguint().unwrap(), 'a'), 
+        (11.to_biguint().unwrap(), 'b'),
+        (12.to_biguint().unwrap(), 'c'), 
+        (13.to_biguint().unwrap(), 'd'), 
+        (14.to_biguint().unwrap(), 'e'), 
+        (15.to_biguint().unwrap(), 'f')  
+    ])
+}
+// Adapted from the rustlings introduction print :) 
+pub fn print_rsa(bit_size: u64){ 
+    println!(r#"  _ _ _ _ _ _ _ _ _ __"#);
+    println!(r#"°/  _ __  ___  ___    \°"#);
+    println!(r#"|  |  __|/ __|/ _ \    |"#);
+    println!(r#"|  | |   \__ \ /_\ \   |"#);
+    println!(r#"|  |_|   |___//   \_\  |"#);
+    println!(r#"+---------[{}]---------+"#, bit_size);
+}
 
 // ALGEBRA ON NUMBERS
+
+pub fn naive_hex_from_biguint(num: BigUint) ->  String{
+    let zero = 0.to_biguint().unwrap();
+    let one = 1.to_biguint().unwrap();
+    let sixteen = 16.to_biguint().unwrap();
+    let mut num2 = num.clone();
+    let mut remainder = num2.modpow(&one, &sixteen);
+    let mut vec = Vec::new();
+    vec.push(remainder.clone());
+    while num2.clone() > zero {
+        num2 = num2 / sixteen.clone();
+        remainder = num2.modpow(&one, &sixteen);
+        if num2.clone() > zero{
+            vec.push(remainder.clone());
+        }
+    }
+    vec.reverse();
+    let mut s = String::new();
+    let hashes = hex_from_uint();
+    for x in &vec{
+        // println!("{x}");
+        s.push(hashes[&x])
+    }
+    s
+}
+
 pub fn gen_rand_inverses_below(num: BigUint) -> (BigUint, BigInt){
     let mut rng = rand::thread_rng();
     loop {
@@ -17,6 +77,7 @@ pub fn gen_rand_inverses_below(num: BigUint) -> (BigUint, BigInt){
         }
     }
 }
+
 fn are_prime_one_another(a: BigUint, b: BigUint) -> (HashMap<BigUint, BigInt>, bool){
     let one : BigUint = One::one();
     let (coefs, gcd) = euclid_algo(a,b);
@@ -77,7 +138,7 @@ pub fn gen_prime(bit_size: u64, k: u32) -> BigUint{
     
 }
 
-fn gen_random_odd_biguint(bit_size: u64) -> BigUint{
+pub fn gen_random_odd_biguint(bit_size: u64) -> BigUint{
     let mut rng = rand::thread_rng();
     let mut num = rng.gen_biguint(bit_size);
     // the lowest bit should be 1 to get an odd number

433ac8fa88e6f9beef2b8f945efa0bdf24f4a64c
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-10-07 17:19:08 +0200

    hexadecimal public keys

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 mod utils;
-use utils::{gen_prime, gen_rand_inverses_below};
-use num_bigint::BigUint;
-use num_traits::One;
+use utils::{gen_prime, gen_rand_inverses_below, print_rsa, naive_hex_from_biguint};
+use num_bigint::{ToBigUint};
 use clap::Parser;
 use std::thread;
 
 #[derive(Parser, Debug)]
 struct Args {
     /// Number of bits of the numbers to generate
-    #[arg(short, long, default_value_t = 500)]
+    #[arg(short, long, default_value_t = 512)]
     bit_size: u64,
 
     /// Number of times to run Miller-Rabin test
@@ -19,8 +18,8 @@ struct Args {
 
 fn main(){
     let args = Args::parse();
-    println!("");
-    println!("Code launched with the following arguments {:#?}", args);
+    // println!("");
+    // println!("Code launched with the following arguments {:#?}", args);
 
     let bit_size: u64= args.bit_size;
     let k_mil_rab: u32 = args.k_mil_rab;
@@ -32,9 +31,16 @@ fn main(){
     );
     let (p,q) = handle.join().unwrap();    
 
-    let one : BigUint = One::one();
+    let one = 1.to_biguint().unwrap();
     let euler_ind = (p.clone()-one.clone()) * (q.clone()-one.clone());
     let (e, _) = gen_rand_inverses_below(euler_ind);
 
-    println!("\nYour public key is the pair [ \nn = {}  \n\ne = {} \n]", p*q, e);
+    print_rsa(bit_size);
+    let n = p*q;
+    println!("\nYour public key in decimal is the pair [ \nn = {}  \n\ne = {} \n]", n, e);
+
+    let n = naive_hex_from_biguint(n);
+    let e = naive_hex_from_biguint(e);
+    println!("\nYour public key in hexadecimal is the pair [ \nn = 0x{}  \n\ne = 0x{} \n]", n, e);
+
 }
